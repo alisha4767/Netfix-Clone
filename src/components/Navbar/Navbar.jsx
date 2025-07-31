04c0078f93ea0ad0ef3fd557546aa5b8c36bb4bf
Alisha shakir <alishashakir86@gmail.com>
2025-07-31 23:37:32 +0530

    navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.png'
-function navbar() {
+import logo from '../../assests/logo.png'
+import search_icon from '../../assests/search_icon.svg'
+import bell_icon from '../../assests/bell_icon.svg'
+import profile_img from '../../assests/profile_img.png'
+import caret_icon from '../../assests/caret_icon.svg'
+
+const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar-left'>
         <img src={logo} alt="" />
         <ul>
           <li>Home</li>
-          <li>Tv sHOWS</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
         </ul>
       </div>
-      <div className='navbar-right'></div>
+      <div className='navbar-right'>
+        <img src={search_icon} alt="" className='icons'/>
+        <p>children</p>
+        <img src={bell_icon} alt="" className='icons'/>
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className='profile'/>
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p>sign out of Netflix</p>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
